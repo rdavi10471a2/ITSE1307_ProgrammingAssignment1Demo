79e649a07f5043b5eea8104096164e5c107ffa31
Richard Davison <rdavi10471a2@gmail.com>
2022-02-04 20:43:00 -0600

    added example printing for assigment 2

--- Pyramid.cpp
+++ Pyramid.cpp
@@ -35,10 +35,37 @@ void Pyramid::readFromConsole() {
 
 void Pyramid::drawMe()
 {
-	cout << "Programming Assignment 2 will do this we hope " << endl;
+    // number of spaces
+    int k = 2 * side - 2;
+    int n = side;
+
+    // Outer loop to handle number of rows
+    // n in this case
+    for (int i = 0; i < n; i++) {
+
+        // Inner loop to handle number spaces
+        // values changing acc. to requirement
+        for (int j = 0; j < k; j++)
+            cout << " ";
+
+        // Decrementing k after each loop
+        k = k - 1;
+
+        // Inner loop to handle number of columns
+        // values changing acc. to outer loop
+        for (int j = 0; j <= i; j++) {
+            // Printing stars
+            cout << "* ";  //space is important
+        }
+
+        // Ending line after each row
+        cout << endl;
+    }
 }
 void Pyramid::outputToConsole()
 {
 	cout << endl;
 	cout <<fixed<<setprecision(3)<< "Pyramid " << name << " with side " << side << " Area: " << getArea() << " Perimiter:" << getPerimiter()<< endl<<endl;
+    drawMe();
+    cout << endl<<endl;
 }
--- main.cpp
+++ main.cpp
@@ -3,24 +3,25 @@
 
 int main() 
 {
-	Pyramid Pyramid1;
-	Pyramid1.readFromConsole();
-	Pyramid1.outputToConsole();
+	//declare and initialize 3 Pyramids
+	Pyramid Pyramid1("Pyramid1",3);
+	//Pyramid1.readFromConsole();  //Assignment 1 read from console assignemnt 2 you are supposed to initialize with constructor
+	Pyramid1.outputToConsole();  //print info and call drawMe();
 
-	Pyramid Pyramid2;
-	Pyramid2.readFromConsole();
-	Pyramid2.outputToConsole();
+	Pyramid Pyramid2("Pyramid2", 6);
+	//Pyramid2.readFromConsole();  //Assignment 1 read from console assignemnt 2 you are supposed to initialize with constructor
+	Pyramid2.outputToConsole(); //print info and call drawMe();
 
-	Pyramid Pyramid3;
-	Pyramid3.readFromConsole();
-	Pyramid3.outputToConsole();
-	
+	Pyramid Pyramid3("Pyramid3", 9);
+	//Pyramid3.readFromConsole();  //Assignment 1 read from console assignemnt 2 you are supposed to initialize with constructor
+	Pyramid3.outputToConsole();  //print info and call drawMe();
+
 	//psuedocode for assignment 2
 	/*
 	int menuchoice = -1;
 	do
 	{
-		//display menu
+		//display menu using cout
 		//read user input choice
 		//choose shape to print using switch
 	
